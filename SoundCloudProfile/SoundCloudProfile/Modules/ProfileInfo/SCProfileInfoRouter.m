02c6b81ee32539e1a060e9450850b49979cc72d4
Pavel Katunin <wk.katunin@gmail.com>
2018-08-19 23:26:43 +0300

    Router

--- SoundCloudProfile/SoundCloudProfile/Modules/ProfileInfo/SCProfileInfoRouter.m
+++ SoundCloudProfile/SoundCloudProfile/Modules/ProfileInfo/SCProfileInfoRouter.m
@@ -1,5 +1,22 @@
 #import "SCProfileInfoRouter.h"
+#import "SCRemoteHTTPService.h"
+#import "SCProfileInfoTableViewController.h"
+#import "SCServices.h"
+#import "SCProfileInfoPresenter.h"
 
 @implementation SCProfileInfoRouter
+
++ (UIViewController<SCProfileInfoViewProtocol> *)createProfileController {
+    SCServices *services = [SCServices shared];
+    
+    SCProfileInfoPresenter *presenter =
+    [[SCProfileInfoPresenter alloc] initWithUserProfileService:services.profileService
+                                                   httpService:services.httpService];
+    SCProfileInfoTableViewController *controller =
+    [[SCProfileInfoTableViewController alloc] initWithStyle:UITableViewStylePlain];
+    controller.presenter = presenter;
+    presenter.view = controller;
+    return controller;
+}
 
 @end
